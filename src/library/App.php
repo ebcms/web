fcf8f62e7a80c25c93e5c9fca17205412ca46aca
John Doe <johndoe@example.com>
2022-03-11 16:39:26 +0800

    1

--- src/library/App.php
+++ src/library/App.php
@@ -4,8 +4,15 @@ declare(strict_types=1);
 
 namespace App\Ebcms\Web;
 
+use App\Ebcms\Web\Middleware\Page;
 use DigPHP\Framework\AppInterface;
+use DigPHP\Framework\Framework;
 
 class App implements AppInterface
 {
+
+    public static function onExecute()
+    {
+        Framework::bindMiddleware(Page::class);
+    }
 }
---
+++ src/library/Middleware/Page.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Ebcms\Web\Middleware;
+
+use DigPHP\Framework\Framework;
+use DigPHP\Psr17\Factory;
+use DigPHP\Template\Template;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\MiddlewareInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+
+class Page implements MiddlewareInterface
+{
+    public function process(
+        ServerRequestInterface $request,
+        RequestHandlerInterface $handler
+    ): ResponseInterface {
+        return Framework::execute(function (
+            Template $template,
+            Factory $factory
+        ) use ($request, $handler): ResponseInterface {
+            if (isset($_SERVER['PATH_INFO']) && $_SERVER['PATH_INFO']) {
+                $path = ltrim($_SERVER['PATH_INFO'], '/');
+                if (preg_match('/^\w+$/', $path)) {
+                    $tpl = $path . '@ebcms/web';
+                    if ($template->getTplFile($tpl)) {
+                        $response = $factory->createResponse();
+                        $response->getBody()->write($template->renderFromFile($tpl));
+                        return $response;
+                    }
+                }
+            }
+            return $handler->handle($request);
+        });
+    }
+}
